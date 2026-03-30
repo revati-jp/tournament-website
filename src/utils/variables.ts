3706bbd8676484cafb28a11923bb574be9237520
Rinrin.rs <rinrin0413.rs@gmail.com>
2026-03-30 21:41:47 +0900

    update(header): ヘッダーアイテム現象に伴って、ハンバーガーボタンへのブレイクポイントを変更

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -5,4 +5,4 @@ export const COPYRIGHT = "© 2026 REVATI";
 // JavaScript 定数 `BREAKPOINT_HB` を更新する際は、`/src/styles/_mixin.scss` 内の Sass 変数 `$bp-hamburger-btn` も同時に更新してください。
 // ※ `BREAKPOINT_HB` の閾値 = `$bp-hamburger-btn` - 1
 /** ハンバーガーボタン用のメディアクエリのブレークポイント。 */
-export const BREAKPOINT_HB = "(max-width: 1250px)";
+export const BREAKPOINT_HB = "(max-width: 1174px)";
