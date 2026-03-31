c2ff2755e07e059102c2df6b82f32a7ea6b7b1af
Rinrin.rs <rinrin0413.rs@gmail.com>
2026-03-31 13:27:28 +0900

    refactor: 各大会専用の TypeScript ユーティリティをサブディレクトリに分離
    関連するインポートパスやドキュメントも更新。

--- src/components/community/gateway/header/MobileDrawer.svelte
+++ src/components/community/gateway/header/MobileDrawer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { BREAKPOINT_HB } from '../../../../utils/variables';
+	import { BREAKPOINT_HB } from '../../../../utils/community/gateway';
 
 	interface NavItem {
 		label: string;
