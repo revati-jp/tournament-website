ffc26344727e41d1972f05fe2597a5bed2273a31
RetoRuto9900K <tutinoko2048@gmail.com>
2026-03-31 11:42:30 +0900

    improve: トーナメントの取得方法を型安全な形に変更

--- src/constants/tournaments.ts
+++ src/constants/tournaments.ts
@@ -1,7 +1,7 @@
 import type { Tournament, TournamentType } from "../types";
 
 // より新しい大会が前に来るようにすること。
-export const TOURNAMENTS: Tournament[] = [
+export const TOURNAMENTS = [
   {
     label: 'REVATI Community CUP "Gateway"',
     slug: "gateway",
@@ -14,7 +14,7 @@ export const TOURNAMENTS: Tournament[] = [
     type: "tmp-cup",
     description: "vol1のdescription",
   },
-];
+] as const satisfies Tournament[];
 
 export function getLatestTournament(): Tournament {
   const latest = TOURNAMENTS[0];
@@ -29,9 +29,18 @@ export function getLatestTournamentByType(type: TournamentType): Tournament {
   return latest;
 }
 
-//TODO - 型安全にする?
-export function getTournamentBySlug(slug: string): Tournament {
-  const tournament = TOURNAMENTS.find((t) => t.slug === slug);
+type GetTournamentsByType<T extends TournamentType> = Extract<
+  (typeof TOURNAMENTS)[number],
+  { type: T }
+>;
+
+export function getTournament<T extends TournamentType>(
+  type: T,
+  slug: GetTournamentsByType<T>["slug"],
+): Tournament {
+  const tournament = TOURNAMENTS.find(
+    (t) => t.type === type && t.slug === slug,
+  );
   if (tournament === undefined)
     throw new Error(`Tournament not found for slug: ${slug}`);
   return tournament;
