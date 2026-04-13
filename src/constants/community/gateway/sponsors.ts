4982ee8f21999b3fbbbe63f6ae3b7bfc2b5f3485
RetoRuto9900K <tutinoko2048@gmail.com>
2026-04-14 01:06:17 +0900

    add: スポンサーを追加

--- src/constants/community/gateway/sponsors.ts
+++ src/constants/community/gateway/sponsors.ts
@@ -1,30 +1,16 @@
 import type { Sponsor } from "@types";
 
+import xrayPadLogo from "@assets/community/gateway/sponsors/x-ray-pad.webp";
+
 export const GATEWAY_SPONSORS: Sponsor[] = [
   {
-    name: "TEST SPONSOR",
+    name: "X-rayPAD",
     description:
-      "勝負の世界である以上、成績が低迷してしまうこともあると思います。そんなときでも私たちは物事の目的達成のために継続的に粘り強く努力することを厭わず 最後までやり遂げる無限のパワー・可能性を秘めています。",
-    logoImage:
-      "https://revati.jp/images/logos/revati/title_full_without-slogan.webp",
-    website: "https://revati.jp",
+      "世界中のトッププレイヤーに愛用されるX-Raypadは、卓越したコントロール性と耐久性を兼ね備えたゲーミングマウスパッドを展開しています。独自の表面加工により、どんな環境下でも変わらぬ精度と情熱を支えます。",
+    logoImage: xrayPadLogo,
+    website: "https://shop.x-raypad.com/",
     socials: {
-      twitter: "https://x.com/revati_jp",
-      instagram: "https://revati.jp",
-      line: "https://revati.jp",
-    },
-  },
-  {
-    name: "TEST SPONSOR 2",
-    description:
-      "勝負の世界である以上、成績が低迷してしまうこともあると思います。そんなときでも私たちは物事の目的達成のために継続的に粘り強く努力することを厭わず 最後までやり遂げる無限のパワー・可能性を秘めています。",
-    logoImage:
-      "https://revati.jp/images/logos/revati/title_full_without-slogan.webp",
-    website: "https://revati.jp",
-    socials: {
-      twitter: "https://x.com/revati_jp",
-      instagram: "https://revati.jp",
-      line: "https://revati.jp",
+      twitter: "https://x.com/XraypadOfficial",
     },
   },
 ];
