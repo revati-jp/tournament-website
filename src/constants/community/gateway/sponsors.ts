2b48028f8da051e488ff2c2bc9ca57dc68d7ede2
RetoRuto9900K <tutinoko2048@gmail.com>
2026-04-23 02:16:20 +0900

    fix: スポンサー情報の修正

--- src/constants/community/gateway/sponsors.ts
+++ src/constants/community/gateway/sponsors.ts
@@ -2,11 +2,11 @@ import type { CardItem } from "@app-types/community/gateway";
 
 import xrayPadLogo from "@assets/community/gateway/sponsors/x-raypad.webp";
 import undercadeLogo from "@assets/community/gateway/sponsors/undercade.webp";
-import eKatsuLogo from "@assets/community/gateway/sponsors/ekatsu.webp";
+// import eKatsuLogo from "@assets/community/gateway/sponsors/ekatsu.webp";
 
 export const GATEWAY_SPONSORS: CardItem[] = [
   {
-    name: "X-rayPAD",
+    name: "X-Raypad",
     description:
       "世界中のトッププレイヤーに愛用されるX-Raypadは、卓越したコントロール性と耐久性を兼ね備えたゲーミングマウスパッドを展開しています。独自の表面加工により、どんな環境下でも変わらぬ精度と情熱を支えます。",
     logoImage: xrayPadLogo,
@@ -25,13 +25,13 @@ export const GATEWAY_SPONSORS: CardItem[] = [
       twitter: "https://x.com/cade_under",
     },
   },
-  {
-    name: "e活",
-    description:
-      "『e活』は熱量のあるコミュニティ大会へ協賛しています。 シーンを創る主催者様、ぜひご活用ください。",
-    logoImage: eKatsuLogo,
-    socials: {
-      twitter: "https://x.com/e_ekatsu",
-    },
-  },
+  // {
+  //   name: "e活",
+  //   description:
+  //     "『e活』は熱量のあるコミュニティ大会へ協賛しています。 シーンを創る主催者様、ぜひご活用ください。",
+  //   logoImage: eKatsuLogo,
+  //   socials: {
+  //     twitter: "https://x.com/e_ekatsu",
+  //   },
+  // },
 ];
